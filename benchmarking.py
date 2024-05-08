2e4ec84a43deeaf94e18aa94336bd134aef534f2
Itsrk8401 <158119038+Itsrk8401@users.noreply.github.com>
2024-05-07 23:54:27 -0500

    Update benchmarking.py

--- benchmarking.py
+++ benchmarking.py
@@ -49,5 +49,36 @@ benchmark_subset_sum()
 benchmark_tsp()
 benchmark_sat()
 
+output : 
+
+SSP - Input Size: 10, Average Time: 0.000000 seconds
+SSP - Input Size: 20, Average Time: 0.000000 seconds
+SSP - Input Size: 30, Average Time: 0.000000 seconds
+SSP - Input Size: 40, Average Time: 0.000000 seconds
+SSP - Input Size: 50, Average Time: 0.000000 seconds
+SSP - Input Size: 60, Average Time: 0.000000 seconds
+SSP - Input Size: 70, Average Time: 0.000000 seconds
+SSP - Input Size: 80, Average Time: 0.000000 seconds
+SSP - Input Size: 90, Average Time: 0.000000 seconds
+SSP - Input Size: 100, Average Time: 0.000000 seconds
+TSP - Number of Cities: 4, Average Time: 0.000000 seconds
+TSP - Number of Cities: 5, Average Time: 0.000000 seconds
+TSP - Number of Cities: 6, Average Time: 0.000000 seconds
+TSP - Number of Cities: 7, Average Time: 0.000000 seconds
+TSP - Number of Cities: 8, Average Time: 0.000000 seconds
+TSP - Number of Cities: 9, Average Time: 0.000000 seconds
+TSP - Number of Cities: 10, Average Time: 0.000000 seconds
+TSP - Number of Cities: 11, Average Time: 0.000000 seconds
+TSP - Number of Cities: 12, Average Time: 0.000000 seconds
+SAT - Number of Variables: 3, Average Time: 0.000000 seconds
+SAT - Number of Variables: 4, Average Time: 0.000000 seconds
+SAT - Number of Variables: 5, Average Time: 0.000000 seconds
+SAT - Number of Variables: 6, Average Time: 0.000000 seconds
+SAT - Number of Variables: 7, Average Time: 0.000000 seconds
+SAT - Number of Variables: 8, Average Time: 0.000000 seconds
+SAT - Number of Variables: 9, Average Time: 0.000000 seconds
+
+
+
 
 
